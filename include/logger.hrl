73ca5a05929e5bf263c79e64f3833e2c410732b0
Ross Schlaikjer <ross@schlaikjer.net>
2017-10-29 14:33:36 -0400

    Nop out low-critical message

--- include/logger.hrl
+++ include/logger.hrl
@@ -1,10 +1,10 @@
 %% logging callbacks
 
--define(PRINT(Format, Args), io:format(Format, Args)).
+-define(PRINT(Format, Args), ok).
 
--define(DEBUG(Format, Args), lager:log(debug, self(), Format, Args)).
+-define(DEBUG(Format, Args), ok).
 
--define(INFO_MSG(Format, Args), lager:log(info, self(), Format, Args)).
+-define(INFO_MSG(Format, Args), ok).
 
 -define(WARNING_MSG(Format, Args), lager:log(warning, self(), Format, Args)).
 
