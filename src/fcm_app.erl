2d8576d9b1f810f95428d675b1d93d8d7ddf01f9
Ross Schlaikjer <ross@schlaikjer.net>
2018-05-29 20:06:34 +0000

    Allow reading pools from app.conf

--- src/fcm_app.erl
+++ src/fcm_app.erl
@@ -6,7 +6,13 @@
 
 -spec start(_, _) -> {ok, pid()}.
 start(_StartType, _StartArgs) ->
-    fcm_sup:start_link().
+    {ok, Pid} = fcm_sup:start_link(),
+    Pools = application:get_env(fcm, pools, []),
+    lists:foreach(
+        fun({PoolName, PoolKey}) -> fcm_sup:start_child(PoolName, PoolKey) end,
+        Pools
+    ),
+    {ok, Pid}.
 
 -spec stop(_) -> ok.
 stop(_State) ->
